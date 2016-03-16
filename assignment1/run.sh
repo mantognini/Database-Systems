fe73f33f82da39d88df2d077985f0480018e4b36
Marco Antognini <antognini.marco@gmail.com>
2016-03-16 16:42:08 +0100

    Add run.sh for assignment 1

---
+++ assignment1/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+        echo "Expected input argument for dataset size" 1>&2
+        exit 1
+fi
+
+hadoop fs -rm -r -f '/user/mantogni/output/out'
+
+sbt package
+
+spark-submit --class "main.scala.Project1" \
+        --master yarn-cluster --num-executors 25 \
+        Project1.jar \
+        hdfs:///datasets/tpch/${1}gb \
+        hdfs:///user/mantogni/output
+
+hadoop fs -cat '/user/mantogni/output/out/*' | sort -n
+
