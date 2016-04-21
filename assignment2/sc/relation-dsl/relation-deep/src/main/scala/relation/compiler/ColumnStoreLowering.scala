ed0aba30ae304d701e7e07787a4fc98536f98e83
Marco Antognini <antognini.marco@gmail.com>
2016-04-21 17:36:32 +0200

    Implement select

--- assignment2/sc/relation-dsl/relation-deep/src/main/scala/relation/compiler/ColumnStoreLowering.scala
+++ assignment2/sc/relation-dsl/relation-deep/src/main/scala/relation/compiler/ColumnStoreLowering.scala
@@ -30,11 +30,11 @@ class ColumnStoreLowering(override val IR: RelationDSLOpsPackaged, override val 
 
 
     // Load the records using the scanner, unrolling the inner loop (which is defined as a function)
-    def loadRecord(index: Var[Int]) = {
+    def loadRecord(index: Var[Int]) =
       for (column <- schema.columns) {
         dsl"${storage(column)}($index) = $scanner.next_string()"
       }
-    }
+
     val i = newVar(dsl"0")
     dsl"""
       while ($scanner.hasNext) {
@@ -51,7 +51,47 @@ class ColumnStoreLowering(override val IR: RelationDSLOpsPackaged, override val 
   }
 
   def relationSelect(relation: Rep[Relation], field: String, value: Rep[String], resultSchema: Schema): LoweredRelation = {
-    ??? // TODO
+    val (size, storage) = getRelationLowered(relation)
+    val schema = getRelationSchema(relation) // Should be the same as resultSchema...
+
+    val predicateStorage = storage(field)
+
+    // Count how many tuples we should keep
+    val newSize = dsl"""
+      var newSize_ = 0
+      for (i <- 0 until $size) {
+        if (${predicateStorage}(i) == $value)
+          newSize_ = newSize_ + 1
+      }
+      newSize_
+    """
+
+    // Create a new storage for the filtered relation
+    val newStorage = (for (column <- schema.columns) yield {
+      val colStorage = dsl"new Array[String]($newSize)"
+
+      column -> colStorage
+    }).toMap
+
+    // Load the data into the new storage, with unrolling of inner loop
+    def copyRecord(source: Var[Int], dest: Var[Int]) =
+      for (column <- schema.columns) {
+        dsl"${newStorage(column)}($dest) = ${storage(column)}($source)"
+      }
+
+    val source = newVar(dsl"0")
+    val dest = newVar(dsl"0")
+    dsl"""
+      while ($source < $size) {
+        if (${predicateStorage}($source) == $value) {
+          ${copyRecord(source, dest)}
+          $dest = $dest + 1
+        }
+        $source = $source + 1
+      }
+    """
+
+    (newSize, newStorage)
   }
 
   def relationJoin(leftRelation: Rep[Relation], rightRelation: Rep[Relation], leftKey: String, rightKey: String, resultSchema: Schema): LoweredRelation = {
